449dd09fbbe42bc26150fcfbcf524303cdd963d7
Kyo <tranducnguyenk37@gmail.com>
2025-02-22 11:35:49 +0700

    reformat

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 const DawnService = require('./dawn.service');
-const {readFile, appendFile} = require('./FileHelper');    
+const { readFile, appendFile } = require('./FileHelper');
 require('dotenv').config();
 
 (async () => {
@@ -9,30 +9,22 @@ require('dotenv').config();
     for (let item of listData) {
 
         const [userName, token, proxy, extensionId, password] = item.split('|');
-        if(!password){
+        if (!password) {
             password = passwordDefault;
         }
-        
+
         const tokenLogin = await DawnService.DoLogin({
             proxy, userName, password, extensionId
         });
-       
-        if(tokenLogin){
+
+        if (tokenLogin) {
             console.log(`Save token account: ${userName}`)
             await appendFile('./data_login.txt', `${userName}|${tokenLogin}|${proxy}|${extensionId}\n`);
-        }else{
+        } else {
             console.log(`Login fail account: ${userName}`)
             listFail.push(item);
         }
     }
 
     await appendFile('./data_fail.txt', listFail.join('\n'));
-
-    // const userName = 'tranducnguyenk37@gmail.com';
-    // const proxy = '45.249.106.92:5789:bmacahrr:a8x7mfciy2yq';
-    // const extensionId = 'fpdkjdnhkakefebpekbdhillbhonfjjp';
-    // const token = await DawnService.DoLogin({
-    //     proxy, userName, password, extensionId
-    // });
-    // await appendFile('./data_login.txt', `${userName}|${token}|${proxy}|${extensionId}`);
 })();
